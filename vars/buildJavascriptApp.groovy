9d3b9aedfc5abeb216d95d0f939266762d1ded9d
izzetcan <izzetcankarakus@gmail.com>
2021-03-28 23:18:47 +0300

    changes

--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -1,8 +1,6 @@
 def call(Map config = [:], Closure body) {
     node {
-        git url: "https://github.com/werne2j/sample-nodejs" stage("Install") {
-            sh "npm install"
-        }
+
         stage("Test") {
             sh "npm test"
         }
