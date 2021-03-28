d8d3f0cb191ddd5035838fdfdcf5166e475a3aa4
izzetcan <izzetcankarakus@gmail.com>
2021-03-28 23:30:23 +0300

    changes

--- src/org/example/Constants.groovy
+++ src/org/example/Constants.groovy
@@ -3,4 +3,9 @@ package org.example;
 class Constants {
     static final String SLACK_MESSAGE = "Sending Slack Notification"
     static final String EMAIL_MESSAGE = "Sending Email"
+    static final ArrayList<String> JOBS = new ArrayList<String>(Arrays.asList(
+            "master",
+            "add_person",
+            "delete_person"
+    ));
 }
