32d783a614aca261f6b48f8c0f670881be5f5a3a
Izzet Can Karakus <izzetcankarakus@gmail.com>
2022-09-18 05:32:51 +0300

    make all variables public

--- src/org/example/Constants.groovy
+++ src/org/example/Constants.groovy
@@ -2,8 +2,8 @@ package org.example;
 
 class Constants {
     public static final String slack_message = "Sending Slack Notification"
-    static final String EMAIL_MESSAGE = "Sending Email"
-    static final ArrayList<String> JOBS = new ArrayList<String>(Arrays.asList(
+    public static final String EMAIL_MESSAGE = "Sending Email"
+    public static final ArrayList<String> JOBS = new ArrayList<String>(Arrays.asList(
             "master",
             "add_person",
             "delete_person"
